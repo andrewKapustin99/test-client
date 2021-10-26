bb47987a1e45fff2c6180ffd6ec31f5bdfec389b
Andrew Kapustin <7280567-andrew_kapustin@users.noreply.gitlab.com>
2021-10-26 23:31:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // const [name, setName] = useState("")
+    // const [age, setAge] = useState(0)
+    // const [country, setCountry] = useState("")
+    // const [position, setPosition] = useState("")
+    // const [wage, setWage] = useState(0)
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const [employeeList, setEmployeeList] = useState([])
+
+
+    const addEmployee = () => {
+        axios.post('https://react-express-sql-app.herokuapp.com/user', {
+           email, 
+           password
+        }).then(() => {
+            console.log('success');
+        })
+    }
+
+    const getEmployees = () => {
+        axios.get('https://react-express-sql-app.herokuapp.com/user')
+            .then((response) => {
+                setEmployeeList(response.data)
+            })
+    }
+
+
+    return (
+        <div className="App">
+            <div className="information">
+                <label >Email:</label>
+                <input type="text" onChange={(e) => { setEmail(e.target.value) }} />
+
+                <label >Password:</label>
+                <input type="password" onChange={(e) => { setPassword(e.target.value) }} />
+
+                <button onClick={addEmployee}>Add Employee</button>
+            </div>
+
+            <div className="employees">
+                <button onClick={getEmployees}>Show employees</button>
+            </div>
+
+            {employeeList.map((val, key) => {   
+                return <div className='employee'>
+                    <h3>{val.email}</h3>
+                    <h3>{val.password}</h3>
+                </div>
+            })}
+        </div>
+    );
 }
 
 export default App;
